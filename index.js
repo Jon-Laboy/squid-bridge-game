10c5f245f68a51249223484816e0943afa8d0e26
Jon-Laboy <jonlaboyoc@gmail.com>
2021-12-02 16:32:37 -0500

    3rd commit

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-const start = document.querySelector('#start')
-const end = document.querySelector('#end')
-const bridge = document.querySelector('#bridge')
+const start = document.querySelector("#start");
+const end = document.querySelector("#end");
+const bridge = document.querySelector("#bridge");
 
 //Create-Bridge
 const createBridge = (gridsNum) => {
-    for (i = 1; i < gridsNum; i++) {
-        let gridDiv = document.createElement("div")
-        gridDiv.id = i;
-        gridDiv.classList.add("grid")
-        bridge.append(gridDiv)
-    };
+  for (i = 1; i < gridsNum; i++) {
+    let gridDiv = document.createElement("div");
+    gridDiv.id = i;
+    gridDiv.classList.add("grid");
+    bridge.append(gridDiv);
+  }
 };
 
-createBridge(13)
+createBridge(13);
 
-//Add Character 
-const character = document.createElement("img")
-character.setAttribute("src", 'img/green.png')
-character.classList.add("character")
+//Add Character
+const character = document.createElement("img");
+character.setAttribute("src", "img/green.png");
+character.classList.add("character");
 
-start.appendChild(character)
-
+start.appendChild(character);
 
 //play again btn click
-function goAgain() { 
-    document.getElementById("deadModal").style.display = 'none'
+const goAgain = () => {
+  document.getElementById("deadModal").style.display = "none";
 }
 
 //Array of "glass" squares
-let tilesArr = [...bridge.childNodes]
+let tilesArr = [...bridge.childNodes];
 
-console.log(tilesArr)
+// console.log(tilesArr[11].value = Math.floor(Math.random() * 2));
 
-const checkIndex = (e) => { 
-    console.log(Array.from(tilesArr).indexOf(e.target))
-}
+//Move character img to index of clicked tile
+const moveCharacter = (e) => {
+  console.log(Array.from(tilesArr).indexOf(e.target));
+  const tileIndex = Array.from(tilesArr).indexOf(e.target);
+  tilesArr[tileIndex].appendChild(character);
+  // if(tileIndex % 2) { 
+  //   tilesArr[tileIndex - 1] DISABLE
+  // }
+ 
+};
 
+//move character
 tilesArr.forEach((tile) => {
-    tile.addEventListener('click', checkIndex);
-  })
-  
+  tile.addEventListener("click", moveCharacter);
+});
 
-// tilesArr[8].appendChild(character)
+//allow character to get to finish line
+end.addEventListener("click", () => end.appendChild(character));
 
-// let idsToNum = tilesArr.map((item) => parseInt(item.id))
+//run Math.random on even or odd indexes in array
 
-console.log(idsToNum)
-// console.log(parseInt(tilesArr[0].id))
 
