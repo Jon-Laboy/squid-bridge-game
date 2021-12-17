8070d5fd996ab96dcba0760e9d8ef0217b94279b
Jon-Laboy <jonlaboyoc@gmail.com>
2021-12-17 14:33:53 -0500

    correct movement added

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const start = document.querySelector("#start");
 const end = document.querySelector("#end");
-const deadModal = document.querySelector("#deadWrapper")
-const winModal = document.querySelector("#winWrapper")
+const deadModal = document.querySelector("#deadWrapper");
+const winModal = document.querySelector("#winWrapper");
 const bridge = document.querySelector("#bridge");
-
 
 //Create-Bridge
 const createBridge = (gridsNum) => {
@@ -28,11 +27,14 @@ start.appendChild(character);
 //play again btn click
 const goAgain = () => {
   deadModal.style.display = "none";
-  window.location.reload()
+  window.location.reload();
 };
 
 //Array of "glass" squares
 let tilesArr = [...bridge.childNodes];
+
+tilesArr[10].classList.replace("grid", "grid2");
+tilesArr[11].classList.replace("grid", "grid2");
 
 //Chunk array into groups of 2
 const sliceIntoChunks = (arr, chunkSize) => {
@@ -46,62 +48,74 @@ const sliceIntoChunks = (arr, chunkSize) => {
 
 const groupedArr = sliceIntoChunks(tilesArr, 2);
 
-//gonna need grouped arr for move blocking i think 
-console.log(groupedArr)
 //set random value for odd indexed tiles
 //set opposite value for even indexed tiles
-  groupedArr.forEach((arr) => { 
-    arr[1].innerText = Math.floor(Math.random() * 2)
-    arr[1].innerText == 0 ? arr[0].innerText = 1 : arr[0].innerText = 0
-  })
-
+groupedArr.forEach((arr) => {
+  arr[1].innerText = Math.floor(Math.random() * 2);
+  arr[1].innerText == 0 ? (arr[0].innerText = 1) : (arr[0].innerText = 0);
+});
 
 //Move character img to index of clicked tile
 const moveCharacter = (e) => {
-  const tileIndex = Array.from(tilesArr).indexOf(e.target);
-  console.log(tileIndex)
-  //if on zero value tile player loses 
-  if(tilesArr[tileIndex].innerText == 0) {
-    deadModal.style.visibility = 'visible' 
-    console.log('lose')
-  }                                     
-  tilesArr[tileIndex].appendChild(character);                      
+  let tileIndex = Array.from(tilesArr).indexOf(e.target);
+  console.log(tileIndex);
 
-  console.log(tilesArr[tileIndex - 2])
-  //if odd index tile (on right side of bridge)
-  // TODO -- maybe while loop? no se. 
-  // if (tileIndex % 2) {
-  //   tilesArr[tileIndex - 1].style.pointerEvents = "none";
-  //   tilesArr[tileIndex + 1].style.pointerEvents = "none";
-  //   tilesArr[tileIndex + 2].style.pointerEvents = "none";
-  //   tilesArr[tileIndex - 4].style.pointerEvents = "none";
-  //   tilesArr[tileIndex - 5].style.pointerEvents = "none";
-  //   tilesArr[tileIndex - 6].style.pointerEvents = "none";
-  // }
-  // //if even index tile (on left side of bridge)
-  // if (tileIndex % 2 == 0) {
-  //   tilesArr[tileIndex + 1].style.pointerEvents = "none";
-  //   tilesArr[tileIndex + 2].style.pointerEvents = "none";
-  //   tilesArr[tileIndex + 3].style.pointerEvents = "none";
-  //   tilesArr[tileIndex - 4].style.pointerEvents = "none";
-  //   tilesArr[tileIndex - 5].style.pointerEvents = "none";
-  //   tilesArr[tileIndex - 6].style.pointerEvents = "none";
-  // }
+  tilesArr[tileIndex].appendChild(character);
+
+  //if on zero value tile player loses
+  if (tilesArr[tileIndex].innerText == 0) {
+    deadModal.style.visibility = "visible";
+  }
+
+  //open next row of tiles to movement
+  if (tileIndex == 10 || tileIndex == 11) {
+    tilesArr[8].classList.replace("grid", "grid2");
+    tilesArr[9].classList.replace("grid", "grid2");
+    console.log("first row");
+    return;
+  }
+
+  if (tileIndex == 8 || tileIndex == 9) {
+    tilesArr[7].classList.replace("grid", "grid2");
+    tilesArr[6].classList.replace("grid", "grid2");
+    console.log("second row");
+    return;
+  }
+
+  if (tileIndex == 6 || tileIndex == 7) {
+    tilesArr[5].classList.replace("grid", "grid2");
+    tilesArr[4].classList.replace("grid", "grid2");
+    console.log("third row");
+    return;
+  }
+
+  if (tileIndex == 4 || tileIndex == 5) {
+    tilesArr[3].classList.replace("grid", "grid2");
+    tilesArr[2].classList.replace("grid", "grid2");
+    console.log("fourth row");
+    return;
+  }
+
+  if (tileIndex == 2 || tileIndex == 3) {
+    tilesArr[1].classList.replace("grid", "grid2");
+    tilesArr[0].classList.replace("grid", "grid2");
+    console.log("fifth row");
+    return;
+  }
+  
+  if (tileIndex == 0 || tileIndex == 1) {
+    //allow character to get to finish line
+    end.addEventListener("click", () => {
+      end.appendChild(character);
+      winModal.style.visibility = "visible";
+    });
+    console.log("sixth row");
+    return;
+  }
 };
-//  function test2(e) { 
-//    console.log(e.target)
-//  }
+
 //move character
 tilesArr.forEach((tile) => {
   tile.addEventListener("click", moveCharacter);
-  // tile.addEventListener("click", test2)
 });
-
-//allow character to get to finish line
-end.addEventListener("click", () => { 
-  end.appendChild(character)
-  winModal.style.visibility = 'visible'
-  console.log('you won!') 
-});
-
 
